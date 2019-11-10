9964c2df978dd2fa6c81fd635c7c96cb4f81913c
hamzakashif902 <55187558+hamzakashif902@users.noreply.github.com>
2019-11-10 11:40:05 +0500

    Add files via upload

---
+++ factorial using recursion.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int fact(int n);
+int fact(int n)
+{
+    if(n > 1)
+        return n * fact(n - 1);
+    else
+        return 1;
+
+}
+int main()
+{
+	int num;
+	cin>>num;
+	cout<<"factorial of number is  "<<fact(num);
+	return 0;
+}
